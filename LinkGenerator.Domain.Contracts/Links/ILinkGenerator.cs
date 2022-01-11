51f375bea4b1a69c81273c6565fd87b80f1539a2
Rebwar Hosein Poori <rebwar.poori@hotmail.com>
2022-01-11 10:54:45 +0330

    Add project files.

---
+++ LinkGenerator.Domain.Contracts/Links/ILinkGenerator.cs
@@ -0,0 +1,18 @@
+﻿using LinkGenerator.Domain.Core.Links;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkGenerator.Domain.Contracts.Links
+{
+   public interface ILinkGenerator
+    {
+        Task CreateLink(string url);
+
+        Task<Link> ShortLinkRequset(string code);
+
+        
+
+    }
+}
---
+++ LinkGenerator.Domain.Core/Links/Link.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkGenerator.Domain.Core.Links
+{
+   public class Link
+    {
+        public int LinkId { get; set; }
+        public string Url { get; set; }
+        public string GenerateCode { get; set; }
+        public long VisitCount { get; set; }
+    }
+}
