3857c463c07f2f52310d45d00e6d0ee81704dabd
Rebwar Hosein Poori <rebwar.poori@hotmail.com>
2022-01-11 11:04:19 +0330

    Create repository and add Local refrences

---
+++ LinkGenerator.Infrastructure.Data/Links/LinkRepository.cs
@@ -0,0 +1,22 @@
+﻿using LinkGenerator.Domain.Contracts.Links;
+using LinkGenerator.Domain.Core.Links;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkGenerator.Infrastructure.Data.Links
+{
+    public class LinkRepository : ILinkGenerator
+    {
+        public Task CreateLink(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Link> ShortLinkRequset(string code)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
